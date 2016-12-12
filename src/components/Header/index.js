e407bfbf11115801c7b9cdf8320a1e49bfeb5eec
Brian Cruikshank <brian@brianswebstudio.com>
2016-12-12 03:51:39 -0500

    convert Header into a component and make header opaque on scroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from "react"
+import React, { PropTypes, Component } from "react"
 import { Link } from "phenomic"
 import Svg from "react-svg-inline"
 
@@ -7,74 +7,101 @@ import gitHubSvg from "../icons/iconmonstr-github-1.svg"
 
 import styles from "./index.css"
 
-const Header = (props, { metadata: { pkg } }) => (
-  <header className={ styles.header }>
-    <div className={ styles.logo }>
-      <Link
-        className={ styles.link }
-        to={ "/" }
-      >
-        { "Acro With Brian" }
-      </Link>
-    </div>
-    <nav className={ styles.nav }>
-      <div className={ styles.navPart1 }>
-        <Link
-          className={ styles.link }
-          to={ "/about" }
-        >
-          { "About" }
-        </Link>
-        <Link
-          className={ styles.link }
-          to={ "/photos" }
-        >
-          { "Photos" }
-        </Link>
-        <Link
-          className={ styles.link }
-          to={ "/blog" }
-        >
-          { "Blog" }
-        </Link>
-        <Link
-          className={ styles.link }
-          to={ "/events" }
-        >
-          { "Events" }
-        </Link>
-        <Link
-          className={ styles.link }
-          to={ "/contact" }
-        >
-          { "Contact" }
-        </Link>
-      </div>
-      <div className={ styles.navPart2 }>
-        {
-          pkg.twitter &&
-          <a
-            href={ `https://twitter.com/${pkg.twitter}` }
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { didScroll: false }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  handleScroll(){
+    if( window.pageYOffset > 50 )
+      this.setState({didScroll: true})
+    else
+      this.setState({didScroll: false})
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+   componentWillUnmount(){
+     window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  render() {
+    const { pkg } = this.context.metadata
+
+    return (
+      <header className={ styles.header + (this.state.didScroll ? ' ' + styles.scrolled : '') }>
+        <div className={ styles.logo }>
+          <Link
             className={ styles.link }
+            to={ "/" }
           >
-            <Svg svg={ twitterSvg } cleanup />
-            { "Twitter" }
-          </a>
-        }
-        {
-          pkg.repository &&
-          <a
-            href={ pkg.repository }
-            className={ styles.link }
-          >
-            <Svg svg={ gitHubSvg } cleanup />
-            { "GitHub" }
-          </a>
-        }
-      </div>
-    </nav>
-  </header>
-)
+            { "Acro With Brian" }
+          </Link>
+        </div>
+        <nav className={ styles.nav }>
+          <div className={ styles.navPart1 }>
+            <Link
+              className={ styles.link }
+              to={ "/about" }
+            >
+              { "About" }
+            </Link>
+            <Link
+              className={ styles.link }
+              to={ "/photos" }
+            >
+              { "Photos" }
+            </Link>
+            <Link
+              className={ styles.link }
+              to={ "/blog" }
+            >
+              { "Blog" }
+            </Link>
+            <Link
+              className={ styles.link }
+              to={ "/events" }
+            >
+              { "Events" }
+            </Link>
+            <Link
+              className={ styles.link }
+              to={ "/contact" }
+            >
+              { "Contact" }
+            </Link>
+          </div>
+          <div className={ styles.navPart2 }>
+            {
+              pkg.twitter &&
+              <a
+                href={ `https://twitter.com/${pkg.twitter}` }
+                className={ styles.link }
+              >
+                <Svg svg={ twitterSvg } cleanup />
+                { "Twitter" }
+              </a>
+            }
+            {
+              pkg.repository &&
+              <a
+                href={ pkg.repository }
+                className={ styles.link }
+              >
+                <Svg svg={ gitHubSvg } cleanup />
+                { "GitHub" }
+              </a>
+            }
+          </div>
+        </nav>
+      </header>
+    )
+  }
+}
 
 Header.contextTypes = {
   metadata: PropTypes.object.isRequired,
