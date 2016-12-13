721cf581bee5d06ee58825a0b17c5f2e9b289630
Brian Cruikshank <brian@brianswebstudio.com>
2016-12-13 02:05:30 -0500

    add mobile menu, refactor

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,8 @@ import React, { PropTypes, Component } from "react"
 import { Link } from "phenomic"
 import Svg from "react-svg-inline"
 
+import handleClickAway from '../../utils/handleClickAway'
+
 import twitterSvg from "../icons/iconmonstr-twitter-1.svg"
 import gitHubSvg from "../icons/iconmonstr-github-1.svg"
 
@@ -10,27 +12,57 @@ import styles from "./index.css"
 class Header extends Component {
   constructor(props) {
     super(props)
-    this.state = { didScroll: false }
+    this.state = {
+      didScroll: false,
+      sideNavOpen: false
+    }
     this.handleScroll = this.handleScroll.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.closeNav = this.closeNav.bind(this)
   }
 
   handleScroll(){
-    if( window.pageYOffset > 50 )
-      this.setState({didScroll: true})
-    else
-      this.setState({didScroll: false})
+    // only change scroll state if menu closed
+    if (!this.state.sideNavOpen) {
+      if( window.pageYOffset > 50 )
+        this.setState({didScroll: true})
+      else
+        this.setState({didScroll: false})
+    }
+  }
+
+  closeNav(e) {
+      const toggleNode = this.refs.toggle
+      const isOutsideClick = handleClickAway(toggleNode, e)
+      if (toggleNode && isOutsideClick) {
+        this.setState({
+          sideNavOpen: false
+        })
+      }
+    }
+    handleClick() {
+      this.setState({
+        sideNavOpen: !this.state.sideNavOpen
+      })
   }
 
   componentDidMount(){
     window.addEventListener('scroll', this.handleScroll);
+    document.body.addEventListener('click', this.closeNav)
   }
 
    componentWillUnmount(){
      window.removeEventListener('scroll', this.handleScroll);
+     document.body.removeEventListener('click', this.closeNav)
   }
 
   render() {
     const { pkg } = this.context.metadata
+//    const { fullWidth } = this.props
+    const { sideNavOpen } = this.state
+    const mobileNav = (sideNavOpen) ? styles.open : ''
+    const openClass = (sideNavOpen) ? styles.animate : ''
+//    const containerStyle = (fullWidth) ? styles.fullWidth : ''
 
     return (
       <header className={ styles.header + (this.state.didScroll ? ' ' + styles.scrolled : '') }>
@@ -42,39 +74,58 @@ class Header extends Component {
             { "Acro With Brian" }
           </Link>
         </div>
-        <nav className={ styles.nav }>
-          <div className={ styles.navPart1 }>
+
+        <div ref='toggle' onClick={this.handleClick} className={styles.toggle}>
+          <div className={styles.ham}>
+            <div className={`${styles.bar} ${openClass}`} />
+          </div>
+        </div>
+
+        <nav className={ `${styles.nav} ${mobileNav}` }>
+          {/* <div className={ styles.navPart1 }> */}
+          <ul className={styles.navItems}>
+            <li className={styles.navItem}>
             <Link
               className={ styles.link }
               to={ "/about" }
             >
               { "About" }
             </Link>
+            </li>
+            <li className={styles.navItem}>
             <Link
               className={ styles.link }
               to={ "/photos" }
             >
               { "Photos" }
             </Link>
+            </li>
+            <li className={styles.navItem}>
             <Link
               className={ styles.link }
               to={ "/blog" }
             >
               { "Blog" }
             </Link>
+            </li>
+            <li className={styles.navItem}>
             <Link
               className={ styles.link }
               to={ "/events" }
             >
               { "Events" }
             </Link>
+            </li>
+            <li className={styles.navItem}>
             <Link
               className={ styles.link }
               to={ "/contact" }
             >
               { "Contact" }
             </Link>
-          </div>
+            </li>
+          </ul>
+          {/* </div> */}
           <div className={ styles.navPart2 }>
             {
               pkg.twitter &&
