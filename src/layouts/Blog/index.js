9d2450e34f7c5cfe2c593da22e9495e48b5a6774
Brian Cruikshank <brian@brianswebstudio.com>
2017-01-14 16:02:58 -0500

    fix blog post spacing

--- src/layouts/Blog/index.js
+++ src/layouts/Blog/index.js
@@ -2,18 +2,20 @@ import React, { PropTypes } from "react"
 
 import Page from "../Page"
 import LatestPosts from "../../components/LatestPosts"
-// import styles from "./index.css"
+import styles from "./index.css"
 
 const Blog = (props) => {
   // const { head } = props;
 
   return (
     <div>
-    <Page
-      { ...props }
-    >
-    </Page>
-    <LatestPosts numberOfPosts="12" />
+      <Page
+        { ...props }
+      >
+      </Page>
+      <div className={ styles.wrapper }>
+        <LatestPosts numberOfPosts="12" />
+      </div>
     </div>
   )
 }
