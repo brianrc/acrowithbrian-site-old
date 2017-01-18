5f5e6bad876b8d029e9c3d38eba612c90f0e5c1e
Brian Cruikshank <brian@brianswebstudio.com>
2017-01-18 17:05:53 -0500

    fix url, try fixing og:image

--- src/layouts/Page/index.js
+++ src/layouts/Page/index.js
@@ -39,7 +39,7 @@ const Page = ({
       content: joinUri(process.env.PHENOMIC_USER_URL, __url),
     },
     { property: "og:description", content: head.description },
-    { property: "og:image", content: joinUri(process.env.PHENOMIC_USER_PATHNAME, __url) + head.hero.split('..').join('') },
+    { property: "og:image", content: joinUri(process.env.PHENOMIC_USER_URL, head.hero) },
     { property: "article:publisher", content: "https://www.facebook.com/brianrc" },
     // { name: "twitter:card", content: "summary" },
     // { name: "twitter:title", content: metaTitle },
